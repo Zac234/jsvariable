34212240dbfe556b60da74273b31a59b0b3e4140
zac234 <zacbenincasa@gamil.com>
2021-11-06 09:18:35 -0600

    added skeleton for project

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var player_username = "Shoebill"
+var number_of_tickets = 99
+
+
+console.log(player_username , number_of_tickets );
+
+if(player_username){
+    number_of_tickets=10< console.log("you can get some gum");    
+}
+    else(player_username)=> 
+{
+  number_of_tickets=30< console.log("you can get a troll doll");
+};
+elseif (player_username);
+{
+    number_of_tickets = 70 < console.log("you can get a fake wig");
+};
+elseif(player_username);
+{
+    number_of_tickets = 100 <console.log("you can geta Blowup hammer");
+}
+
+
+
